b41d6966ca2781d982f57b91822a8b9d1479469a
salaarhamza <45125770+salaarhamza@users.noreply.github.com>
2026-06-20 21:42:09 +0200

    Add parametrized login tests for SauceDemo users

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,38 @@
-from pages.login_page import LoginPage
-from utils.test_data import BASE_URL, VALID_USER, PASSWORD
+import pytest
 
-def test_valid_login(page):
+from pages.login_page import LoginPage
+from utils.test_data import (
+    BASE_URL,
+    VALID_USERS,
+    PASSWORD,
+    LOCKED_USER,
+    INVALID_USER,
+    INVALID_PASSWORD,
+    INVALID_LOGIN_ERROR,
+    LOCKED_OUT_ERROR
+)
+
+@pytest.mark.parametrize("username", VALID_USERS)
+def test_valid_login(page, username):
     login_page = LoginPage(page)
     
     login_page.navigate(BASE_URL)
-    login_page.login(VALID_USER, PASSWORD)
-
-
-    assert page.url == "https://www.saucedemo.com/inventory.html"
+    login_page.login(username, PASSWORD)
     assert login_page.is_inventory_page_loaded()
+    assert "inventory.html" in page.url
+
+def test_invalid_login_error_message(page):
+    login_page = LoginPage(page)
+
+    login_page.navigate(BASE_URL)
+    login_page.login(INVALID_USER, INVALID_PASSWORD)
+
+    assert login_page.is_error_message_visible()
+    assert INVALID_LOGIN_ERROR == login_page.get_error_message()
+
+def test_locked_out_user_shows_error_message(page):
+    login_page = LoginPage(page)
+    login_page.navigate(BASE_URL)
+    login_page.login(LOCKED_USER, PASSWORD)
+    assert login_page.is_error_message_visible()
+    assert login_page.get_error_message() == LOCKED_OUT_ERROR
--- utils/test_data.py
+++ utils/test_data.py
@@ -1,8 +1,16 @@
 BASE_URL = "https://www.saucedemo.com/"
 
-VALID_USER = "standard_user"
+VALID_USERS = [
+    "standard_user",
+    "problem_user",
+    "performance_glitch_user",
+    "error_user",
+    "visual_user",
+]
+
 LOCKED_USER = "locked_out_user"
 PASSWORD = "secret_sauce"
-
 INVALID_USER = "wrong_user"
 INVALID_PASSWORD = "wrong_password"
+INVALID_LOGIN_ERROR = "Epic sadface: Username and password do not match any user in this service"
+LOCKED_OUT_ERROR = "Epic sadface: Sorry, this user has been locked out."
