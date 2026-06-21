8f6e1ad59d893dc00474460e7dc1a1450ed09eeb
salaarhamza <45125770+salaarhamza@users.noreply.github.com>
2026-06-21 23:44:53 +0200

    Expand inventory regression test coverage

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -5,15 +5,13 @@ class InventoryPage:
         self.inventory_container = ".inventory_list"
         self.inventory_items = ".inventory_item"
         self.product_names = ".inventory_item_name"
+        self.product_descriptions = ".inventory_item_desc"
         self.product_prices = ".inventory_item_price"
+        self.product_images = ".inventory_item_img img"
         self.sort_dropdown = "[data-test='product-sort-container']"
+        self.cart_link = ".shopping_cart_link"
+        self.cart_badge = ".shopping_cart_badge"
 
-    def is_inventory_page_loaded(self):
-        return self.page.is_visible(self.inventory_container)
-
-    def get_product_names(self):
-        return self.page.locator(self.product_names).all_text_contents()
-        
     def is_loaded(self):
         return self.page.locator(self.inventory_container).is_visible()
 
@@ -22,10 +20,15 @@ class InventoryPage:
 
     def get_product_names(self):
         return self.page.locator(self.product_names).all_inner_texts()
+    def get_product_descriptions(self):
+        return self.page.locator(self.product_descriptions).all_inner_texts()
 
     def get_product_prices(self):
         price_texts = self.page.locator(self.product_prices).all_inner_texts()
         return [float(price.replace("$", "")) for price in price_texts]
+
+    def get_product_image_count(self):
+        return self.page.locator(self.product_images).count()
 
     def sort_by_name_a_to_z(self):
         self.page.select_option(self.sort_dropdown, "az")
@@ -38,17 +41,34 @@ class InventoryPage:
 
     def sort_by_price_high_to_low(self):
         self.page.select_option(self.sort_dropdown, "hilo")
-        
-    
-    #### Cart interactions ####
+
     def add_backpack_to_cart(self):
         self.page.click("[data-test='add-to-cart-sauce-labs-backpack']")
 
     def remove_backpack_from_cart(self):
         self.page.click("[data-test='remove-sauce-labs-backpack']")
 
-    def open_cart(self):
-        self.page.click(".shopping_cart_link")
+    def add_bike_light_to_cart(self):
+        self.page.click("[data-test='add-to-cart-sauce-labs-bike-light']")
+
+    def add_all_products_to_cart(self):
+        add_buttons = self.page.locator("button[data-test^='add-to-cart']")
+        count = add_buttons.count()
+
+        for _ in range(count):
+            add_buttons.first.click()
 
     def get_cart_badge_count(self):
-        return self.page.locator(".shopping_cart_badge").inner_text()
+        return self.page.locator(self.cart_badge).inner_text()
+
+    def is_backpack_remove_button_visible(self):
+        return self.page.locator("[data-test='remove-sauce-labs-backpack']").is_visible()
+
+    def open_backpack_detail_by_name(self):
+        self.page.click("[data-test='item-4-title-link']")
+
+    def open_backpack_detail_by_image(self):
+        self.page.click("#item_4_img_link")
+
+    def open_cart(self):
+        self.page.click(self.cart_link)
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -2,6 +2,10 @@ from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from utils.test_data import BASE_URL, VALID_USERS, PASSWORD
 
+from pages.login_page import LoginPage
+from pages.inventory_page import InventoryPage
+from utils.test_data import BASE_URL, VALID_USERS, PASSWORD
+
 
 def login_as_standard_user(page):
     login_page = LoginPage(page)
@@ -9,38 +13,23 @@ def login_as_standard_user(page):
     login_page.login(VALID_USERS[0], PASSWORD)
 
 
-def test_inventory_page_loads_after_login(page):
+def test_all_products_render_with_required_information(page):
     login_as_standard_user(page)
     inventory_page = InventoryPage(page)
 
-    assert inventory_page.is_loaded()
-
-
-def test_inventory_contains_six_products(page):
-    login_as_standard_user(page)
-    inventory_page = InventoryPage(page)
+    names = inventory_page.get_product_names()
+    descriptions = inventory_page.get_product_descriptions()
+    prices = inventory_page.get_product_prices()
 
     assert inventory_page.get_product_count() == 6
+    assert len(names) == 6
+    assert len(descriptions) == 6
+    assert len(prices) == 6
+    assert inventory_page.get_product_image_count() == 6
 
-
-def test_all_products_have_names(page):
-    login_as_standard_user(page)
-    inventory_page = InventoryPage(page)
-
-    product_names = inventory_page.get_product_names()
-
-    assert len(product_names) == 6
-    assert all(name.strip() for name in product_names)
-
-
-def test_all_products_have_prices(page):
-    login_as_standard_user(page)
-    inventory_page = InventoryPage(page)
-
-    product_prices = inventory_page.get_product_prices()
-
-    assert len(product_prices) == 6
-    assert all(price > 0 for price in product_prices)
+    assert all(name.strip() for name in names)
+    assert all(description.strip() for description in descriptions)
+    assert all(price > 0 for price in prices)
 
 
 def test_sort_products_by_name_a_to_z(page):
@@ -53,6 +42,16 @@ def test_sort_products_by_name_a_to_z(page):
     assert product_names == sorted(product_names)
 
 
+def test_sort_products_by_name_z_to_a(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.sort_by_name_z_to_a()
+    product_names = inventory_page.get_product_names()
+
+    assert product_names == sorted(product_names, reverse=True)
+
+
 def test_sort_products_by_price_low_to_high(page):
     login_as_standard_user(page)
     inventory_page = InventoryPage(page)
@@ -61,3 +60,82 @@ def test_sort_products_by_price_low_to_high(page):
     product_prices = inventory_page.get_product_prices()
 
     assert product_prices == sorted(product_prices)
+
+
+def test_sort_products_by_price_high_to_low(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.sort_by_price_high_to_low()
+    product_prices = inventory_page.get_product_prices()
+
+    assert product_prices == sorted(product_prices, reverse=True)
+
+
+def test_add_one_product_to_cart_updates_badge_and_button(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.add_backpack_to_cart()
+
+    assert inventory_page.get_cart_badge_count() == "1"
+    assert inventory_page.is_backpack_remove_button_visible()
+
+
+def test_add_multiple_products_to_cart_updates_badge_count(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.add_backpack_to_cart()
+    inventory_page.add_bike_light_to_cart()
+
+    assert inventory_page.get_cart_badge_count() == "2"
+
+
+def test_remove_product_from_inventory_updates_badge_count(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.add_backpack_to_cart()
+    inventory_page.add_bike_light_to_cart()
+    inventory_page.remove_backpack_from_cart()
+
+    assert inventory_page.get_cart_badge_count() == "1"
+
+
+def test_add_all_products_to_cart_updates_badge_count(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.add_all_products_to_cart()
+
+    assert inventory_page.get_cart_badge_count() == "6"
+
+
+def test_clicking_product_name_opens_product_detail_page(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.open_backpack_detail_by_name()
+
+    assert "inventory-item.html?id=4" in page.url
+    assert page.locator(".inventory_details_name").inner_text() == "Sauce Labs Backpack"
+
+
+def test_clicking_product_image_opens_product_detail_page(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.open_backpack_detail_by_image()
+
+    assert "inventory-item.html?id=4" in page.url
+    assert page.locator(".inventory_details_name").inner_text() == "Sauce Labs Backpack"
+
+
+def test_cart_icon_navigates_to_cart_page(page):
+    login_as_standard_user(page)
+    inventory_page = InventoryPage(page)
+
+    inventory_page.open_cart()
+
+    assert "cart.html" in page.url
